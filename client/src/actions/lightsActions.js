661f078d5b6d1b464aa61fddf2d693ec37f59eb8
[cccarle] <[ce222qw@student.lnu.se]>
2019-03-21 10:24:25 +0100

    changes

--- client/src/actions/lightsActions.js
+++ client/src/actions/lightsActions.js
@@ -3,10 +3,10 @@ const axios = require('axios');
 export const handleLights = (value) => {
 
     // Make a request for a user with a given ID
-    axios.get('http://localhost:8080/')
+    axios.get('http://localhost:8080')
         .then(function (response) {
             // handle success
-            console.log(response);
+            console.log(response)
         })
         .catch(function (error) {
             // handle error
