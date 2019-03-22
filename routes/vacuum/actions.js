e127b1778f280fab4fc6c9de304374a1d63e954a
joel <zorcey@gmail.com>
2019-03-22 16:47:55 +0100

    making state for vacuum

--- routes/vacuum/actions.js
+++ routes/vacuum/actions.js
@@ -1,46 +1,20 @@
 module.exports = (server, vacuum) => {
   server.put('/things/vacuum/actions/state', async (req, res, next) => {
+    let result
     try {
       let clean = req.body.clean
+      let dock = req.body.dock
       console.log(clean)
       // let result = await vacuum.clean()
       if (clean === false) {
-
+        result = await vacuum.stop()
       }
-      let result = 'executed'
-      console.log(result)
-      res.send(result)
-    } catch (e) {
-      console.log(e)
-      res.send(e)
-    }
-    next()
-  })
-  server.put('/things/vacuum/actions/clean', async (req, res, next) => {
-    try {
-      let result = await vacuum.clean()
-      console.log(result)
-      res.send(result)
-    } catch (e) {
-      console.log(e)
-      res.send(e)
-    }
-    next()
-  })
-  server.put('/things/vacuum/actions/stop', async (req, res, next) => {
-    try {
-      let result = await vacuum.stop() // TODO: This doesnt even work
-      console.log(result)
-      res.send(result)
-    } catch (e) {
-      console.log(e)
-      res.send(e)
-    }
-    next()
-  })
-  server.put('/things/vacuum/actions/dock', async (req, res, next) => {
-    try {
-      let result = await vacuum.dock() // TODO: This doesnt even work
+      if (clean) {
+        result = await vacuum.clean()
+      }
+      if (dock) {
+        result = await vacuum.dock()
+      }
       console.log(result)
       res.send(result)
     } catch (e) {
